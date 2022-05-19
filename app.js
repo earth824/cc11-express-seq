bad204a9166ea7e33e2ae16270d309ac6c90bc95
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-05-19 09:29:23 +0700

    add passport jwt

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 require('dotenv').config();
+require('./config/passport');
 
 const express = require('express');
+const cors = require('cors');
 
 const todoRoute = require('./routes/todoRoute');
 const userRoute = require('./routes/userRoute');
 const notFoundMiddleware = require('./middlewares/notfound');
 const errorMiddleware = require('./middlewares/error');
 const authenticate = require('./middlewares/authenticate');
+const passportJwt = require('./middlewares/passportJwt');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // REST API: handle resource Todos
 // CREATE, UPDATE, DELETE, GETALL, GETBYID
-app.use('/todos', authenticate, todoRoute);
+app.use('/todos', passportJwt, todoRoute);
 
 // REST API: handle resource Users
 // CREATE, UPDATE
